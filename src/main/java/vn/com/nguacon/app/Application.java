356932c0920f7b56e977f1a0a15ea4f5e5ba736a
minh.toquang <minh.toquang@vndirect.com.vn>
2015-02-12 11:45:27 +0700

    first commit

---
+++ src/main/java/vn/com/nguacon/app/Application.java
@@ -0,0 +1,17 @@
+package vn.com.nguacon.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("vn.com.nguacon")
+public class Application {
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+ 
+}
---
+++ src/main/java/vn/com/nguacon/config/WebMvcConfig.java
@@ -0,0 +1,77 @@
+package vn.com.nguacon.config;
+
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+
+import org.springframework.boot.context.embedded.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+	@Controller
+	static class Routes {
+		@RequestMapping({ "/home", "/about" })
+		public String index() {
+			return "forward:/index.html";
+		}
+	}
+
+	@Bean
+	public ServletContextInitializer servletContextInitializer() {
+		return servletContext -> {
+			final CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+			characterEncodingFilter.setEncoding("UTF-8");
+			characterEncodingFilter.setForceEncoding(false);
+			servletContext.addFilter("characterEncodingFilter",
+					characterEncodingFilter).addMappingForUrlPatterns(
+					EnumSet.of(DispatcherType.REQUEST), false, "/*");
+		};
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/webjars/**").addResourceLocations(
+				"classpath:/META-INF/resources/webjars/");
+	}
+
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setTemplateResolver(templateResolver());
+		return engine;
+	}
+
+	@Bean
+	public ClassLoaderTemplateResolver templateResolver() {
+		ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+		templateResolver.setTemplateMode("XHTML");
+		templateResolver.setPrefix("static/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setOrder(0);
+		return templateResolver;
+	}
+
+	@Bean
+	public ViewResolver viewResolver() {
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine());
+		viewResolver.setCharacterEncoding("UTF-8");
+		viewResolver.setViewNames(new String[] { "*" });
+		viewResolver.setOrder(1);
+		return viewResolver;
+	}
+}
